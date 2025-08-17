11f7b928c8560e781a0f01f2a129b6c30fd61df0
Vluurie <vluurie@gmx.net>
2025-08-17 06:06:57 +0200

    testtt

--- src/software_renderer/overlay/d3d.rs
+++ src/software_renderer/overlay/d3d.rs
@@ -110,7 +110,7 @@ pub fn create_shared_texture_and_get_handle(
             },
             Usage: D3D11_USAGE_DEFAULT,
 
-            BindFlags: D3D11_BIND_RENDER_TARGET.0 as u32,
+            BindFlags: D3D11_BIND_RENDER_TARGET.0 as u32 | D3D11_BIND_SHADER_RESOURCE.0 as u32,
             CPUAccessFlags: 0,
             MiscFlags: D3D11_RESOURCE_MISC_SHARED_KEYEDMUTEX.0 as u32,
         };

d64e8b99ef54f2215662c0da4bb1fbaf1554b26c
Vluurie <vluurie@users.noreply.github.com>
2025-08-31 02:11:18 +0200

    add depth stencil and split primitive tick and UI tick in seperate methods

--- src/software_renderer/d3d11_compositor/traits.rs
+++ src/software_renderer/d3d11_compositor/traits.rs
@@ -1,8 +1,9 @@
 use directx_math::XMMatrix;
-use windows::Win32::Graphics::Direct3D11::ID3D11DeviceContext;
+use windows::Win32::Graphics::Direct3D11::{ ID3D11DepthStencilView, ID3D11DeviceContext };
 pub struct FrameParams<'a> {
     pub context: &'a ID3D11DeviceContext,
     pub view_projection_matrix: &'a XMMatrix,
+    pub depth_stencil_view: &'a Option<ID3D11DepthStencilView>,
     pub screen_width: f32,
     pub screen_height: f32,
     pub time: f32,
